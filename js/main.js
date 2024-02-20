53834de1bd7ec118099e0f54d1f93e374369c4d1
voss <snape525severus@gmail.com>
2024-02-20 22:13:14 +0800

    done sign in

--- js/main.js
+++ js/main.js
@@ -417,58 +417,58 @@ document.getElementById('scrollToTop').addEventListener('click', function() {
  /*----------------------------------------*/
  /* 21. Modal Menu Active
  /*----------------------------------------*/ 
- $('.product-details-images').each(function(){
-     var $this = $(this);
-     var $thumb = $this.siblings('.product-details-thumbs, .tab-style-left');
-     $this.slick({
-        arrows: false,
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        autoplay: false,
-        autoplaySpeed: 5000,
-        dots: false,
-        infinite: true,
-        centerMode: false,
-        centerPadding: 0,
-        asNavFor: $thumb,
-    });
- });
- $('.product-details-thumbs').each(function(){
-     var $this = $(this);
-     var $details = $this.siblings('.product-details-images');
-     $this.slick({
-        slidesToShow: 4,
-        slidesToScroll: 1,
-        autoplay: false,
-        autoplaySpeed: 5000,
-        dots: false,
-        infinite: true,
-        focusOnSelect: true,
-        centerMode: true,
-        centerPadding: 0,
-        prevArrow: '<span class="slick-prev"><i class="fa fa-angle-left"></i></span>',
-        nextArrow: '<span class="slick-next"><i class="fa fa-angle-right"></i></span>',
-        asNavFor: $details,
-    });
- });
- $('.tab-style-left, .tab-style-right').each(function(){
-     var $this = $(this);
-     var $details = $this.siblings('.product-details-images');
-     $this.slick({
-        slidesToShow: 3,
-        slidesToScroll: 1,
-        autoplay: false,
-        autoplaySpeed: 5000,
-        dots: false,
-        infinite: true,
-        focusOnSelect: true,
-        vertical: true,
-        centerPadding: 0,
-        prevArrow: '<span class="slick-prev"><i class="fa fa-angle-down"></i></span>',
-        nextArrow: '<span class="slick-next"><i class="fa fa-angle-up"></i></span>',
-        asNavFor: $details,
-    });
- });
+//  $('.product-details-images').each(function(){
+//      var $this = $(this);
+//      var $thumb = $this.siblings('.product-details-thumbs, .tab-style-left');
+//      $this.slick({
+//         arrows: false,
+//         slidesToShow: 1,
+//         slidesToScroll: 1,
+//         autoplay: false,
+//         autoplaySpeed: 5000,
+//         dots: false,
+//         infinite: true,
+//         centerMode: false,
+//         centerPadding: 0,
+//         asNavFor: $thumb,
+//     });
+//  });
+//  $('.product-details-thumbs').each(function(){
+//      var $this = $(this);
+//      var $details = $this.siblings('.product-details-images');
+//      $this.slick({
+//         slidesToShow: 4,
+//         slidesToScroll: 1,
+//         autoplay: false,
+//         autoplaySpeed: 5000,
+//         dots: false,
+//         infinite: true,
+//         focusOnSelect: true,
+//         centerMode: true,
+//         centerPadding: 0,
+//         prevArrow: '<span class="slick-prev"><i class="fa fa-angle-left"></i></span>',
+//         nextArrow: '<span class="slick-next"><i class="fa fa-angle-right"></i></span>',
+//         asNavFor: $details,
+//     });
+//  });
+//  $('.tab-style-left, .tab-style-right').each(function(){
+//      var $this = $(this);
+//      var $details = $this.siblings('.product-details-images');
+//      $this.slick({
+//         slidesToShow: 3,
+//         slidesToScroll: 1,
+//         autoplay: false,
+//         autoplaySpeed: 5000,
+//         dots: false,
+//         infinite: true,
+//         focusOnSelect: true,
+//         vertical: true,
+//         centerPadding: 0,
+//         prevArrow: '<span class="slick-prev"><i class="fa fa-angle-down"></i></span>',
+//         nextArrow: '<span class="slick-next"><i class="fa fa-angle-up"></i></span>',
+//         asNavFor: $details,
+//     });
+//  });
 /*----------------------------------------*/
 /* 22. Cart Plus Minus Button
 /*----------------------------------------*/
@@ -541,6 +541,7 @@ document.getElementById('scrollToTop').addEventListener('click', function() {
 /* 27. Sign Out
 /*----------------------------------------*/
 function signOut() {
+	sessionStorage.removeItem('user');
 	sessionStorage.removeItem('userId');
 	sessionStorage.removeItem('username');
 	window.location.href = 'sign-in.html';
